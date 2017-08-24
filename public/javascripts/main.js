c910e5d4a8981c3728470b1a5767452ba8fcd6e6
valerio <valerio.cestarelli@datalytics.it>
2017-08-24 12:29:22 +0200

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by valerio on 23/08/17.
+ */
+var d3 = require('d3');
+var $ = require('jquery');
+
+var container = $(".map-container");
+alert("The container... " + container.toString());
