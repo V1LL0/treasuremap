60797f41ede3a7f87b637c906d7f0b24401089c5
valerio <valerio.cestarelli@datalytics.it>
2017-08-28 21:59:03 +0200

    base

--- public/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by valerio on 23/08/17.
- */
-var d3 = require('d3');
-var $ = require('jquery');
-
-var container = $(".map-container");
-alert("The container... " + container.toString());
